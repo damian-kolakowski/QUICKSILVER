7078a900958b5ae5fc6c764f02d6cb46785160a5
bkleiner <b.kleiner@str8labs.com>
2023-01-06 17:47:49 +0100

    turtle_mode: engage when arm comes _after_ turtle.

--- src/flight/turtle_mode.c
+++ src/flight/turtle_mode.c
@@ -25,7 +25,7 @@ static uint8_t turtle_axis = 0;
 static uint8_t turtle_dir = 0;
 
 void turtle_mode_start() {
-  if (!flags.turtle_ready && flags.on_ground && turtle_state == TURTLE_STAGE_IDLE && (state.GEstG.yaw < 0)) {
+  if (!flags.turtle_ready && flags.on_ground && turtle_state == TURTLE_STAGE_IDLE) {
     // only enable turtle if we are onground and not recovering from a interrupted turtle
     flags.turtle_ready = 1;
   }
@@ -55,31 +55,34 @@ void turtle_mode_update() {
 
   switch (turtle_state) {
   case TURTLE_STAGE_IDLE: {
-    // quad was just armed and upside down, begin the turtle sequence
-    static uint8_t last_armed_state_turtle = 0;
-    if (flags.arm_switch != last_armed_state_turtle) {
-      last_armed_state_turtle = flags.arm_switch;
+    if (flags.turtle_ready) {
+      // turtle was marked ready. check if the arm switch was flicked
 
-      // quad was just armed and upside down, begin the turtle sequence
-      if (flags.turtle_ready && flags.arm_switch && (state.GEstG.yaw < 0)) {
-        motor_set_direction(MOTOR_REVERSE);
-        turtle_state = TURTLE_STAGE_START;
+      static uint8_t last_armed_state_turtle = 0;
+      if (flags.arm_switch != last_armed_state_turtle) {
+        last_armed_state_turtle = flags.arm_switch;
+
+        // quad was just armed and upside down, begin the turtle sequence
+        if (flags.arm_switch && (state.GEstG.yaw < 0)) {
+          motor_set_direction(MOTOR_REVERSE);
+          turtle_state = TURTLE_STAGE_START;
+        }
       }
     }
     break;
   }
   case TURTLE_STAGE_START:
-    flags.controls_override = 1;
-    flags.motortest_override = 1;
-    state.rx_override.axis[0] = 0;
-    state.rx_override.axis[1] = 0;
-    state.rx_override.yaw = 0;
-    state.rx_override.axis[3] = 0;
-
     if (!motor_direction_change_done()) {
       // wait for the motor to sucessfully change
       break;
     }
+
+    flags.controls_override = 1;
+    flags.motortest_override = 1;
+    state.rx_override.axis[0] = 0;
+    state.rx_override.axis[1] = 0;
+    state.rx_override.axis[2] = 0;
+    state.rx_override.axis[3] = 0;
 
     if (fabsf(state.rx.axis[0]) > 0.5f || fabsf(state.rx.axis[1]) > 0.5f) {
       if (fabsf(state.rx.axis[0]) < fabsf(state.rx.axis[1])) {
